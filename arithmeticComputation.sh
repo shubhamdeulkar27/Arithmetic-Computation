1ca6aa334090b73b3a3b3c0e7c135312268f08cd
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-24 19:47:37 +0530

    Refactored: Storing Results to Dicitionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,8 +5,12 @@ read -p "Enter Input 1 : " a
 read -p "Enter Input 2 : " b
 read -p "Enter Input 3 : " c
 
-#COMPUTATIONS
-result1=$(( $a+$b*$c ))
-result2=$(( $a*$b+$c ))
-result3=$(( $c+$a/$b ))
-results=$(( $a%$b+$c ))
+#PERFORMING COMPUTATIONS AND STORING RESULTS TO DICTIONARY
+declare -A results
+index=0
+results[$(( index++ ))]=$(( $a+$b*$c ))
+results[$(( index++ ))]=$(( $a*$b+$c ))
+results[$(( index++ ))]=$(( $c+$a/$b ))
+results[$(( index++ ))]=$(( $a%$b+$c ))
+
+
