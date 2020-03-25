7f7456fe495a492ee164fab103972ad80b2abc5f
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-03-25 11:27:21 +0530

    Refactored: Sorting Array in Descending Order

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,6 +5,24 @@ read -p "Enter Input 1 : " a
 read -p "Enter Input 2 : " b
 read -p "Enter Input 3 : " c
 
+#FUNCTION TO SORT ARRAY
+function sortDsc(){
+	array=("$@")
+	for (( i=0 ; i<$(( ${#array[@]}-1 )) ; i++ ))
+	do
+		for (( j=i+1 ; j<${#array[@]} ; j++ ))
+		do
+			if (( ${array[$i]}<${array[$j]} ))
+			then
+				temporary=${array[$i]}
+				array[i]=${array[$j]}
+				array[j]=$temporary
+			fi
+		done
+	done
+echo ${array[@]}
+}
+
 #PERFORMING COMPUTATIONS AND STORING RESULTS TO DICTIONARY
 declare -A results
 index=0
@@ -19,3 +37,6 @@ do
 	resultArray[i]=${results[$i]}
 done
 
+#SORTING ARRAY
+resultArray=$( sortDsc ${resultArray[@]} )
+echo ${resultArray[@]}
